256116e8c12ed39786f377f06b3dda558c4f91ea
Liam DeVoe <orionldevoe@gmail.com>
2018-11-09 18:50:59 -0500

    use str.format for logging

--- main.py
+++ main.py
@@ -78,30 +78,32 @@ def main():
 			except ServerError as e:
 				log.warning("Server error in submission stream: {}. Reddit likely under heavy load, ignoring".format(str(e)))
 			except json.decoder.JSONDecodeError as e:
-				log.warning("JSONDecode Exception in submission stream: {}.".format(str(e)))
+				log.warning("JSONDecode exception in submission stream: {}.".format(str(e)))
+
 	except KeyboardInterrupt:
-			log.info("Received SIGINT, terminating")
-			sys.exit()
+		log.info("Received SIGINT, terminating")
+		sys.exit()
+
 		
 
 def process_submission(submission, shouldComment, shouldFlair):
 	link = "https://old.reddit.com" + submission.permalink
 	if(submission_exists(submission.id)): # Already processed; praw returns the past 100 results for streams, previously iterated over or not
-		log.debug("Submission %s is already processed", submission.id)
+		log.debug("Submission {} is already processed".format(submission.id))
 		return
 
 	log.debug("")
-	log.info("Processing submission %s", link)
-	log.debug("Adding post %s to db", submission.id)
+	log.info("Processing submission {}".format(link))
+	log.debug("Adding post {} to db".format(submission.id))
 	add_submission(submission.id)
 
 
 	title = submission.title.lower()
-	log.debug("Lowered title: %s", title)
+	log.debug("Lowered title: {}".format(title))
 	
 	title_data = TITLE_MATCH.match(title)
 	if(not title_data): # regex didn't match
-		log.debug("Replying malformatted to post %s", submission.id)
+		log.debug("Replying malformatted to post {}".format(submission.id))
 		if(REPLY_MALFORMAT_COMMENT and shouldComment):
 			reply(submission, REPLY_MALFORMAT_COMMENT + REPLY_INFO)
 		return
@@ -112,28 +114,28 @@ def process_submission(submission, shouldComment, shouldFlair):
 	player = info[0].strip() # take from gamemode to first pipe, remove leading + trailing spaces
 	offense = info[-1] # the last occurence. Identical to info[1] usually, 
 					   # but when there's no more pipes (ie title is "[osu!std] tybug") info[1] will throw IOOB
-	log.debug("Gamemode, player, offense: [%s, %s, %s]", gamemode, player, offense)
+	log.debug("Gamemode, player, offense: [{}, {}, {}]".format(gamemode, player, offense))
 
 
 	# Flair it
 	flair_data = parse_flair_data(offense)
 	if(flair_data):
 		if(submission.link_flair_text == "Resolved"): # don't overwrite resolved flairs
-			log.debug("Neglecting to flair submission %s as %s, it is already flaired resolved", submission.id, flair_data[0])
+			log.debug("Neglecting to flair submission {} as {}, it is already flaired resolved".format(submission.id, flair_data[0]))
 		elif shouldFlair:
 			submission.mod.flair(flair_data[0], flair_data[1])
 
 
 
 	if([i for i in title.split(" ") if i in REPLY_IGNORE]): # if the title has any blacklisted words (for discssion threads), don't process it further
-		log.debug("Not processing %s further; the title contained blacklisted discussion words", link)
+		log.debug("Not processing {} further; the title contained blacklisted discussion words".format(link))
 		return
 	
 
 
 	player_data = parse_user_data(player, gamemode, "string")
 	if(player_data is None): # api gives empty json - possible misspelling or user was already restricted
-		log.debug("User with name %s was already restricted at the time of submission, not processing further", player)
+		log.debug("User with name {} was already restricted at the time of submission, not processing further".format(player))
 		if(REPLY_ALREADY_RESTRICTED and shouldComment):
 			log.debug("Leaving already banned comment")
 			reply(submission, REPLY_ALREADY_RESTRICTED.format(USERS + player) + REPLY_INFO)
@@ -144,7 +146,8 @@ def process_submission(submission, shouldComment, shouldFlair):
 		log.debug("User with id {} and name {} already exists".format(player_id, player))
 		previous_id = post_from_user(player_id)
 		previous_submission = reddit.submission(id=previous_id)
-		if(previous_submission.selftext == "[deleted]"): # not foolproof by any means - RE https://www.reddit.com/r/redditdev/comments/44a7xm/praw_how_to_tell_if_a_submission_has_been_removed/, but good enough for us
+		# not foolproof by any means - RE https://www.reddit.com/r/redditdev/comments/44a7xm/praw_how_to_tell_if_a_submission_has_been_removed/, but good enough for us
+		if(previous_submission.selftext == "[deleted]"): 
 			log.debug("previous submission at {} was deleted, removing user {} so a new entry can be placed".format(previous_id, player_id))
 			remove_user(player_id) # so we can add the newer post in a further half dozen lines	
 		else:
@@ -154,14 +157,14 @@ def process_submission(submission, shouldComment, shouldFlair):
 
 
 
-	log.debug("Replying with data for %s", player)
+	log.debug("Replying with data for {}".format(player))
 	if shouldComment:
 		reply(submission, create_reply(player_data, gamemode))
 
 
 	offense_data = parse_offense_type(offense) # take rest of title and make it into single offense
-	log.debug("Adding user with name %s, id %s, post id %s, offense %s, blatant? %s, reported by %s", player, player_id,
-				 submission.id, offense_data[0], offense_data[1], submission.author.name)
+	log.debug("Adding user with name {}, id {}, post id {}, offense {}, blatant? {}, reported by {}".format(player, player_id,
+				 submission.id, offense_data[0], offense_data[1], submission.author.name))
 	# we can assume the id isn't in there already (avoiding UNIQUE_CONSTRAINT) because the if(user_exists) check returns or deletes it
 	add_user(player_id, submission.id, submission.created_utc, offense_data[0], offense_data[1], submission.author.name)
 
@@ -184,7 +187,7 @@ def check_banned(shouldFlair):
 	log.debug("Checking restricted users and new messages..")
 	
 	for data in get_all_users():
-		# log.debug("Checking if user %s is restricted", data[0]) TODO replace with lower level .trace
+		# log.debug("Checking if user {} is restricted".format(data[0])) TODO replace with lower level .trace
 		id = data[0] # user id
 		post_id = data[1] # post id
 		post_date = data[2] # post submission date
@@ -195,10 +198,10 @@ def check_banned(shouldFlair):
 		post_date = int(float(post_date))
 		difference = datetime.datetime.utcnow() - datetime.datetime.fromtimestamp(post_date)
 		if(difference.total_seconds() > LIMIT_DAYS * 24 * 60 * 60): # compare seconds
-			log.info("Removing user %s from database, over time limit", id)
+			log.info("Removing user {} from database, over time limit".format(id))
 			remove_user(id)
-			log.debug("Adding not-restricted statistic for user %s on post %s, reported at %s, restricted at %s, reported for %s, blatant? %s, reported by %s",
-				 			id, post_id, post_date, "n/a", offense_type, blatant, reportee)
+			log.debug("Adding not-restricted statistic for user {} on post {}, reported at {}, restricted at {}, reported for {}, blatant? {}, reported by {}"
+					.format(id, post_id, post_date, "n/a", offense_type, blatant, reportee))
 			add_stat(id, post_id, post_date, "n/a", offense_type, blatant, reportee)
 			continue
 
@@ -206,14 +209,14 @@ def check_banned(shouldFlair):
 		user_data = parse_user_data(id, "0", "id") # gamemode doesn't matter here since we're just checking for empty response
 
 		if(user_data is None): # user was restricted
-			log.info("Removing user %s from database, user restricted", id)
+			log.info("Removing user {} from database, user restricted".format(id))
 			remove_user(id)
 			post = praw.models.Submission(reddit, post_id) # get praw post from id, to flair
-			log.info("Flairing post %s as resolved", post.id)
+			log.info("Flairing post {} as resolved".format(post.id))
 			if(shouldFlair):
 				post.mod.flair("Resolved", "resolved")																						
-				log.debug("Adding restricted statistic for user %s on post %s, reported at %s, restricted at %s, reported for %s, blatant? %s, reported by %s",
-				 			id, post_id, post_date, time.time(), offense_type, blatant, reportee)
+				log.debug("Adding restricted statistic for user {} on post {}, reported at {}, restricted at {}, reported for {}, blatant? {}, reported by {}"
+				 			.format(id, post_id, post_date, time.time(), offense_type, blatant, reportee))
 												# current utc time
 				add_stat(id, post_id, post_date, time.time(), offense_type, blatant, reportee)
 
